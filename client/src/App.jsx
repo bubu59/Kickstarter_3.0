7e6248cdb8390deb40e938c28dcc65af97d02a9d
Joshua Wee <joshua@me3.io>
2023-03-16 17:45:39 +0800

    chore: fixed img issue sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ export default function Home() {
      <div className="sm:flex hidden mr-10 relative">
       <Sidebar/>
      </div>
-     <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+     <div className="flex-1 max-sm:w-full max-w-[1480px] mx-auto sm:pr-5">
       <Navbar/>
      </div>
     </div>
