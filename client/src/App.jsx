53b0f97ede8dedd60c147d2986b40afeb1db928a
Joshua Wee <joshua@me3.io>
2023-03-16 16:41:53 +0800

    chore: setting up folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import {Route, Routes} from 'react-router-dom'
 
-const App = () => {
-  return <div>App</div>;
-};
-
-export default App;
+export default function Home() {
+  return (
+    <div className="container relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+     <div className="sm:flex hidden mr-10 relative">
+      Sidebar
+     </div>
+     <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+      Navbar
+     </div>
+    </div>
+  );
+}
