70ad76754fc25deb38bfacd551e4a5af485144b9
Joshua Wee <joshua@me3.io>
2023-03-16 17:12:00 +0800

    chore: fixing sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import {Route, Routes} from 'react-router-dom'
+import {Sidebar, Navbar} from './components'
 
 export default function Home() {
   return (
     <div className="container relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
      <div className="sm:flex hidden mr-10 relative">
-      Sidebar
+      <Sidebar/>
      </div>
      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-      Navbar
+      <Navbar/>
      </div>
     </div>
   );
